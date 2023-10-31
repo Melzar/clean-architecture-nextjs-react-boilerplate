408b626abef0863ad89fdd48748ff67c870a312e
Tomasz Matuszewski <tomasumat@gmail.com>
2023-10-31 14:45:42 +0100

    NEXT-5 - setting up basic structure

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+
+import styles from '@/app/home/page.module.scss'
+
+export default function Home() {
+  return (
+    <main className={`flex min-h-screen items-center justify-center p-24 ${styles.container}`}>
+      <div className={'flex items-center"'}>Hello World</div>
+    </main>
+  )
+}
